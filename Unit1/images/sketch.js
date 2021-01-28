ab54d7f1685c4b569f490c91fa2c786070af17ad
Rosenkreuz <tsterbenc@gmail.com>
2021-01-28 11:40:37 -0600

    yeet

---
+++ Unit1/images/sketch.js
@@ -0,0 +1,23 @@
+var drg, xiv, mns, zr; //declares the aforementioned as variables
+//'let' also works in place of 'var'
+
+function setup() {
+  createCanvas(800, 800);
+  drg = loadImage("assets/DRGlogo.jpg"); // These commands
+  xiv = loadImage("assets/FFXIVlogo.png"); // load the images
+  mns = loadImage("assets/MNSlogo.png"); // into the program
+  zr = loadImage("assets/ZRlogo.jpg"); // for another command to call
+
+  imageMode(CENTER);
+  /* Remove comment if these are needed later.
+  rectMode(CENTER);
+  textAlign(CENTER);
+  */
+}
+
+function draw() {
+background('white');
+image(drg, width/2, height/2, 200, 200);
+image(xiv, width/2, height/2 - 200, 200, 200);
+image(mns, width/2, height/2 + 200, 200, 200);
+}

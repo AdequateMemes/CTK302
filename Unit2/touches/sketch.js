497184400f88536ed7fe0d0316dffe0dc65d8827
Rosenkreuz <tsterbenc@gmail.com>
2021-02-25 19:43:17 -0600

    make it work lmao

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,11 +1,11 @@
-let numberOfTouches ;
+let numberOfTouches;
 var one, two, three;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  one = loadImage("assets/onefinger.jpg");
-  two = loadImage("assets/twofingers.jpg");
-  three = loadImage("assets/threefingers.jpg");
+  one = loadImage("assets/1.png");
+  two = loadImage("assets/2.png");
+  three = loadImage("assets/3.png");
   imageMode(CENTER);
 }
 
@@ -14,25 +14,25 @@ function draw() {
   numberOfTouches = touches.length;
   text(numberOfTouches + ' touches', 5, 10);
 
-  switch(numberOfTouches) {
+  switch (numberOfTouches) {
     case 0:
-      text("Nary a finger to be found.", 5, 22) ;
-      break ;
+      text("Nary a finger to be found.", 5, 22);
+      break;
 
-      case 1:
-       text("Oh, there's one.", 5, 22) ;
-      // put a picture here
-      break ;
+    case 1:
+      text("Oh, there's one.", 5, 22);
+      image(one, width/2, height/2);
+      break;
 
-      case 2:
-      text("And that makes two.", 5, 22) ;
-            // put a picture here
-      break ;
+    case 2:
+      text("And that makes two.", 5, 22);
+      image(two, width/2, height/2);
+      break;
 
-      case 3:
-     text("A third has joined.", 5, 22) ;
-            // put a picture here
-      break ;
+    case 3:
+      text("A third has joined.", 5, 22);
+      image(three, width/2, height/2);
+      break;
 
 
   }

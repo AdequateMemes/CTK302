6fd234f9b74a2f8010d10d27ab3aae22bb43bcd5
Rosenkreuz <tsterbenc@gmail.com>
2021-04-24 18:59:26 -0500

    weather done

--- Unit4/weather/sketch.js
+++ Unit4/weather/sketch.js
@@ -5,14 +5,19 @@ var weatherID = 0; // returned in the JSON weather element
 var myState = 0;
 var x = 0;
 var windspeed = 0 ;
+var cloud, thermometer, frame;
 
 
 function setup() {
   createCanvas(600, 500);
+  cloud = loadImage("assets/cloud.png");
+  thermometer = loadImage("assets/thermometer2.png");
+  frame = loadImage("assets/frame2.png");
 
   // HERE is the call to get the weather.
 
-  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Champaign,IL,US&units=imperial&';
+  //var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Champaign,IL,US&units=imperial&'; // City,state
+  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?zip=60430,us&units=imperial&'; // Zipcode
 
   //You can also use "zipcode" - var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61820,us&units=imperial&';
 
@@ -46,30 +51,34 @@ function draw() {
       break;
 
     case 1:
-      background(200);
+      textSize(20);
+      background(150, 150, 255);
       fill('black');
-      text(weather.name + "'s weather: " + desc, 20, 20);
-      text("windspeed is " + windspeed, 20, 40);
-      text("temperature is " + temp + " F", 20, 60);
-      text("it feels like it's " + feelslike + " F", 20, 80);
+      text(weather.name + "'s weather: " + desc, 20, 50);
+      text("windspeed is " + windspeed + " mph", 20, 75);
+      text("temperature is " + temp + " F", 20, 100);
+      text("it feels like it's " + feelslike + " F", 20, 125);
 
 
       // cloud
-      fill('white');
       noStroke();
-      ellipse(x, 300, 200, 100);
+      image(cloud, x, 300);
 
       // move the cloud's x position
       x = x + (windspeed/4);
-      if (x > width) x = 0;
+      if (x > width) x = -250;
 
       // thermometer
       fill('red');
 
-      var t = map(temp, -10, 100, 0, height-10);
-      rect(width-width/8, height-height/8, 20, -t);
+      //var t = map(temp, -10, 100, 0, height-10);
+      var t = temp*4;
+      rect(536, 428, 34, -t);
+      image(thermometer, 456, 8);
+      image(frame, 0, 0);
 
       break;
+
 
   }
 }
